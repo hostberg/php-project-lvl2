4084bd9d4fccc9744199904f80a5f32c1b4fce11
hostberg <noreply@hostberg.io>
2022-07-21 15:34:43 +0400

    delete legacy code and comments

--- src/Differ.php
+++ src/Differ.php
@@ -2,16 +2,11 @@
 
 namespace Differ\Differ;
 
-use function Differ\Parsers\parseFile;
+use function Differ\Parser\parseFile;
 use function Differ\Data\getDiff;
 
 function genDiff(string $file1Path, string $file2Path): void
 {
-//    $file1Data = genData(parseFile(realpath($file1Path)));
-//    $file2Data = genData(parseFile(realpath($file2Path)));
-
     $file1Data = parseFile(realpath($file1Path));
     $file2Data = parseFile(realpath($file2Path));
-
-    print_r(getDiff($file1Data, $file2Data));
 }

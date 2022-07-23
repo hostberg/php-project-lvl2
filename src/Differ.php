fdf0630f127e78c207cb3d76226ab58849b07ef7
hostberg <noreply@hostberg.io>
2022-07-23 08:14:23 +0400

    fix stylish formatter pretty print

--- src/Differ.php
+++ src/Differ.php
@@ -6,7 +6,7 @@ use function Differ\Parser\parseFile;
 use function Differ\Data\getDiff;
 use function Differ\Formatter\Formatter\formatData;
 
-function genDiff(string $file1Path, string $file2Path, string $format = 'json'): string
+function genDiff(string $file1Path, string $file2Path, string $format = 'stylish'): string
 {
     $file1Data = parseFile(realpath($file1Path));
     $file2Data = parseFile(realpath($file2Path));
